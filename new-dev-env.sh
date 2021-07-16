50c3a177ba6bccd7045a28bda734d84d8dff60e4
Yibo Cai <cyb70289@gmail.com>
2021-07-16 10:02:54 +0000

    new-dev-env: hints to install docker and golang

--- new-dev-env.sh
+++ new-dev-env.sh
@@ -16,10 +16,15 @@ sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
-mkdir -p ~/share
-chmod 777 ~/share
-
-docker build -t cyb-dev-base .
-
-echo ==========================================================================
+echo
+echo === To install docker ====================================================
+echo ~/dockerfiles/dev/base/helper/install-docker.sh all
+echo
+echo === To install golang ====================================================
+echo ~/dockerfiles/dev/base/helper/install-golang.sh [version]
+echo
+echo === To build dev docker ==================================================
+echo mkdir -p ~/share
+echo chmod 777 ~/share
+echo docker build -t cyb-dev-base ~/dockerfiles/dev/base
 echo docker run -id --name cyb-dev --hostname cyb-dev --network host --cap-add SYS_ADMIN --cap-add=SYS_PTRACE --security-opt apparmor=unconfined --security-opt seccomp=unconfined --shm-size=256m -v /var/run/docker.sock:/var/run/docker.sock -v ~/share:/home/cyb/share cyb-dev-base
